f18b245d25677cf0cf59bf9409bd21d630f4d73b
hlcjunior <62648013+hlcjunior@users.noreply.github.com>
2025-03-08 14:52:29 -0300

    Criando lista e cadastro de lista de emails.

---
+++ resources/views/email-list/create.blade.php
@@ -0,0 +1,9 @@
+<x-layouts.app>
+    <x-slot name="header">
+        <x-h2>{{ __('Email List') }} > {{ __('Create new List') }}</x-h2>
+    </x-slot>
+
+    <x-card>
+        //Formulário de criação
+    </x-card>
+</x-layouts.app>
